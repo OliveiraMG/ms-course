6c65c4c1ad161d1cadbce1ef7f5003dff04c566c
OliveiraMG <matheus.moura@sptech.school>
2024-08-29 17:20:25 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/oliveira/hrpayroll/resources/PaymentResource.java
@@ -0,0 +1,26 @@
+package com.oliveira.hrpayroll.resources;
+
+import com.oliveira.hrpayroll.entities.Payment;
+import com.oliveira.hrpayroll.services.PaymentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/payments")
+public class PaymentResource {
+
+    private PaymentService service;
+
+    public PaymentResource(PaymentService service) {
+        this.service = service;
+    }
+
+    @GetMapping(value = "/{workerId}/days/{days}")
+    public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
+        Payment payment = service.getPayment(workerId, days);
+        return ResponseEntity.ok(payment);
+    }
+}
